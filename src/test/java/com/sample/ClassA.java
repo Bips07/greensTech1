0795ea86b0d237b7348e74fc0b9b3055ee7abb89
Bipin <bipinev@outlook.com>
2025-07-21 19:37:22 +0530

    added 3 methods in class a

--- src/test/java/com/sample/ClassA.java
+++ src/test/java/com/sample/ClassA.java
@@ -14,5 +14,17 @@ public class ClassA {
 	private void test3() {
 		System.out.println("Test3");
 	}
+	
+	private void test6() {
+		System.out.println("Test6");
+	}
+	
+	private void test7() {
+		System.out.println("Test7");
+	}
+	
+	private void test8() {
+		System.out.println("Test8");
+	}
 
 }
